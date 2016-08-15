f2a0ede312366fb22ec3655bffc036b6a349a9f1
Michael Kuehn <mkuehn10@gmail.com>
2016-08-15 12:43:53 -0400

    change to using fetch api

--- apps/app.js
+++ apps/app.js
@@ -1,4 +1,25 @@
 $(function() {
+    /**
+     * XHR wrapped in a promise
+     * @param  {String} url - The URL to fetch.
+     * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
+     */
+    function get(url) {
+        return fetch(url, {
+            method: 'get'
+        });
+    }
+
+    /**
+     * Performs an XHR for a JSON and returns a parsed JSON response.
+     * @param  {String} url - The JSON URL to fetch.
+     * @return {Promise}    - A promise that passes the parsed JSON response.
+     */
+    function getJSON(url) {
+        return get(url).then(function(response) {
+            return response.json();
+        });
+    }
 
     $('form').submit(function(event) {
         event.preventDefault();
@@ -30,17 +51,23 @@ $(function() {
             order: 'relevance'
         };
 
-        $.ajax({
-                url: 'https://www.googleapis.com/youtube/v3/search',
-                data: params,
-                dataType: 'json'
-            })
-            .done(function(data) {
-                showResults(data.items);
-            })
-            .fail(function(jqXHR, error) {
-                console.log('something went wrong');
-            });
+        var url = 'https://www.googleapis.com/youtube/v3/search?' + $.param(params);
+
+        getJSON(url).then(function(response) {
+            showResults(response.items);
+        });
+
+        // $.ajax({
+        //         url: 'https://www.googleapis.com/youtube/v3/search',
+        //         data: params,
+        //         dataType: 'json'
+        //     })
+        //     .done(function(data) {
+        //         showResults(data.items);
+        //     })
+        //     .fail(function(jqXHR, error) {
+        //         console.log('something went wrong');
+        //     });
     };
 
     var showResults = function(results) {
